ac8c9cc014e55d42008f295d9cdfff7377b559e4
Brett Chalupa <brettchalupa@gmail.com>
2023-10-01 15:24:47 -0400

    feat: create initial version of don
    it allows for creating posts on an instance from the CLI

---
+++ README.md
@@ -0,0 +1,21 @@
+# don
+
+Simple Mastodon CLI for creating posts. Built with Deno.
+
+Usage:
+
+```console
+don post "Hello from don!"
+```
+
+When you run `don` for the first time, it will prompt you to set up an
+application on your instance to get an access token to make requests with.
+
+## Bugs / Features
+
+- Only allows you to post to Mastodon and nothing else
+- Text-only
+
+## Thanks
+
+- Masto.js makes it easy to work with Mastodon: https://www.npmjs.com/package/masto
---
+++ don.ts
@@ -0,0 +1,87 @@
+import { createRestAPIClient } from "npm:masto@6.3.1";
+
+interface DonConfig {
+  instanceURL: string;
+  accessToken: string;
+}
+
+const configLocation = Deno.env.get("HOME") + "/.don.json";
+
+let config: DonConfig = {
+  instanceURL: "",
+  accessToken: "",
+};
+
+try {
+  config = JSON.parse(Deno.readTextFileSync(configLocation)) as DonConfig;
+} catch (err) {
+  if (!(err instanceof Deno.errors.NotFound)) {
+    throw err;
+  }
+
+  console.log(`don configuration at ${configLocation} not detected.`);
+  console.log(`beginning interactive don configuration`);
+
+  const instanceURL = prompt(
+    "Please enter your Mastodon instance URL (ex: https://mastodon.social):"
+  );
+  if (!instanceURL) throw new Error("Missing instanceURL");
+
+  confirm(
+    `You must create a new application for don with default permissions at ${instanceURL}/settings/application. Confirm when done.`
+  );
+  const accessToken = prompt("Please enter your access token for don:");
+
+  if (!accessToken) throw new Error("Missing accessToken");
+
+  config = {
+    instanceURL: instanceURL,
+    accessToken: accessToken,
+  };
+
+  Deno.writeTextFileSync(configLocation, JSON.stringify(config, null, 2));
+}
+
+function signout() {
+  Deno.removeSync(configLocation);
+  console.log("Signed out of don.");
+}
+
+async function post(content: string) {
+  config;
+
+  const masto = createRestAPIClient({
+    url: config.instanceURL,
+    accessToken: config.accessToken,
+  });
+
+  const status = await masto.v1.statuses.create({
+    status: content,
+    visibility: "public",
+  });
+
+  console.log("Post created:", status.url);
+  return;
+}
+
+const command = Deno.args[0];
+
+if (command === undefined) {
+  console.log("don is a simple CLI for interacting with Mastodon.");
+  console.log('Create a post with:\n\tdon post "Hello, world!"');
+  console.log("Sign out with:\n\tdon signout");
+} else {
+  switch (command) {
+    case "signout": {
+      signout();
+      break;
+    }
+    case "post":
+    case "p": {
+      await post(Deno.args[1]);
+      break;
+    }
+  }
+}
+
+Deno.exit();
